d9115d2cd1d4c8cee97fdd8b6c88b4916360e0cc
Jon Haddad <jon@jonhaddad.com>
2015-02-06 11:50:58 -0800

    default time

--- bin/fill_kafka.py
+++ bin/fill_kafka.py
@@ -1,4 +1,4 @@
-from sys import path
+from sys import path, argv
 path.append("")
 
 import time
@@ -13,8 +13,18 @@ from webapp.models import PageViews
 sites = [str(uuid4()) for x in range(10)]
 pages = ["/index.html", "/archive.php", "/whatever.js", "/something.css"]
 
+try:
+    per_second = float(argv[1])
+except:
+    print "Using default of 5 / sec"
+    per_second = 5.0
+
+rate = 1 / per_second
+
+print rate
+
 for x in range(100):
     print PageViews.create(random.choice(sites),
                            random.choice(pages),
                            None)
-    time.sleep(.2)
+    time.sleep(rate)
