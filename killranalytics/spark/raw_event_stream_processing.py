04dec394fc9fb5ad56b22072e66867b90c795452
Jon Haddad <jon@jonhaddad.com>
2015-04-29 16:39:18 -0700

    stream is correctly processing

--- killranalytics/spark/raw_event_stream_processing.py
+++ killranalytics/spark/raw_event_stream_processing.py
@@ -5,6 +5,7 @@ from pyspark.sql import SQLContext
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from uuid import uuid1
 
 import json
 
@@ -25,13 +26,15 @@ kafka_stream = KafkaUtils.createStream(stream, \
                                        "raw-event-streaming-consumer",
                                         {topic:1})
 
-kafka_stream.pprint()
-
 # (None, u'{"site_id": "02559c4f-ec20-4579-b2ca-72922a90d0df", "page": "/something.css"}')
 parsed = kafka_stream.map(lambda (k, v): json.loads(v))
 
 # aggregate page views by site
-summed = parsed.map(lambda event: (event['site_id'], 1)).reduceByKey(lambda x,y: x + y)
+summed = parsed.map(lambda event: (event['site_id'], 1)).\
+                reduceByKey(lambda x,y: x + y).\
+                map(lambda x: {"site_id": x[0], "ts": str(uuid1()), "pageviews": x[1]})
+
+summed.pprint()
 summed.saveToCassandra("killranalytics", "real_time_data")
 
 stream.start()
